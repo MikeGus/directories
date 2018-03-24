62a1bb39240d0059af55c771b9f81e1ff8ab1651
MikeGus <vfc17@yandex.ru>
2018-03-24 16:28:29 +0300

    Reworked for chdir

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <list>
 #include <sys/stat.h>
+#include <unistd.h>
 
 const int PROPER_ARGC = 2;
 
@@ -12,14 +13,7 @@ struct Directory_state {
 };
 
 void print_filename(const size_t level, const std::string& fullpath) {
-
-    std::string print_path(fullpath);
-    if (print_path.length() > 1 && print_path.back() == '/') {
-        print_path.back() = '\0';
-    }
-
-    std::cout << std::string(level, '\t') << std::string(print_path, print_path.rfind('/')) << std::endl;
-
+    std::cout << std::string(level, '\t') << fullpath << std::endl;
 }
 
 int show_directory_tree(const char* initial_path) {
@@ -32,56 +26,47 @@ int show_directory_tree(const char* initial_path) {
     state.dp = nullptr;
     state.path = initial_path;
 
-    if (lstat(state.path.c_str(), &statbuf) < 0) {
-        return -1;
-    }
-    if (S_ISDIR(statbuf.st_mode) == 0) {
-        std::cout << state.path << std::endl;
-        return 0;
-    }
-
-    state.dp = opendir(state.path.c_str());
-    if (state.dp == nullptr) {
-        return -1;
-    }
-
-    print_filename(0, state.path);
-
     stack.push_back(state);
+
     while (!stack.empty()) {
         state = stack.back();
         stack.pop_back();
 
-        if (lstat(state.path.c_str(), &statbuf) < 0) {
-            continue;
-        }
+        bool directory = true;
 
-        if (S_ISDIR(statbuf.st_mode) == 0) {
-            print_filename(stack.size(), state.path);
-        } else {
-            if (state.path.back() != '/') {
-                state.path += "/";
-            }
-            if (state.dp == nullptr) {
-                state.dp = opendir(state.path.c_str());
+        if (state.dp == nullptr) {
+            if (lstat(state.path.c_str(), &statbuf) < 0) {
+                directory = false;
+            } else {
                 print_filename(stack.size(), state.path);
-                if (state.dp == nullptr) {
-                    return -1;
+                if (S_ISDIR(statbuf.st_mode) != 0) {
+                    state.dp = opendir(state.path.c_str());
+                    if (state.dp == nullptr) {
+                        return -1;
+                    }
+                    chdir(state.path.c_str());
+                } else {
+                    directory = false;
                 }
             }
-
-            while ((dirp = readdir(state.dp)) != nullptr) {
+        }
+        if (directory) {
+            bool add = false;
+            while (!add && (dirp = readdir(state.dp)) != nullptr) {
                 std::string d_name(dirp->d_name);
-
                 if (d_name != "." && d_name != "..") {
                     stack.push_back(state);
 
                     state.dp = nullptr;
-                    state.path += d_name;
+                    state.path = d_name;
 
                     stack.push_back(state);
-                    break;
+                    add = true;
                 }
+            }
+
+            if (!add) {
+                chdir("..");
             }
         }
     }
